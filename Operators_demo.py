06b883e5475ef7a3de88e55015342c8c1604c1d2
Burpally Bhavana <145525476+burpallybhavana@users.noreply.github.com>
2025-09-17 22:16:49 +0530

    Add files via upload

---
+++ Operators_demo.py
@@ -0,0 +1,31 @@
+a, b = map(int, input().split())
+#Arithmetic Operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a % b)
+print("Floor Division:", a // b)
+print("True Division:",a / b)
+print("Exponent:", a ** b)
+
+#Assignment Operators
+print("Add and Assign:", a + b)
+print("Subtract and Assign:", a - b)
+print("Multiply and Assign:", a * b)
+print("Divide and Assign:", a / b)
+print("Floor Division and Assign:",  a // b)
+print("Modulus and Assign:", a % b)
+print("Exponent and Assign:", a ** b)
+
+#Relational Operators
+print("Greater than:", a > b)
+print("Less than:", a < b)
+print("Greater than or equal to:", a >= b)
+print("Less than or equal to:", a <= b)
+print("Equals to:", a == b)
+print("Not Equals to:", a != b)
+
+#Logical Operators
+print("Logical AND:", a > 5 and b > 5)
+print("Logical OR:", a > 5 or b > 5)
+print("Logical NOT:", not(a > 5 and b > 5))
